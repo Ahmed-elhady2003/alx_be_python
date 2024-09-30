bd913ecd8ca9122e3ab00c687ae3f11a42bfced6
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-09-30 21:54:16 +0300

    ppoppo

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+
+
+class BankAccount:
+    def __init__(self,account_balance):
+        self.account_balance = 0
+    
+    def deposit(amount):
+        account_balance +=amount
+
+    def withdraw(amount):
+        if amount > account_balance:
+            account_balance -=amount 
+            return True
+        else:
+            return False
+
+
+
+
+
+
+    def display_balance():
+        print(f"Current Balance: ${account_balance}")
