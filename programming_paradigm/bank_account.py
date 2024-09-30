8e5af53e56933cf8b901890f5ec1fd6f980a3c5e
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-09-30 22:21:04 +0300

    ppoppo

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,7 +8,7 @@ class BankAccount:
         self.account_balance +=amount
 
     def withdraw(self,amount):
-        if amount > account_balance:
+        if 0<amount > self.account_balance:
             self.account_balance -=amount 
             return True
         else:
