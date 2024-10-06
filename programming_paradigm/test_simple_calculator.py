1f24bee3c40bb71242ef089526c3bce0fc390947
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-10-06 14:36:15 +0300

    ppppp

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,10 +1,17 @@
 from simple_calculator import SimpleCalculator
 import unittest
-class test_calculator(unittest.TestCase):
-    def test_addation(self):
-        self.assertEqual(SimpleCalculator.add(5,9),14)
-        self.assertEqual(SimpleCalculator.add(-8,9),1)
-        self.assertEqual(SimpleCalculator.add(12558,95236),12558+ 95236)
+
+class TestCalculator(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(SimpleCalculator.add(5, 9), 14)
+        self.assertEqual(SimpleCalculator.add(-8, 9), 1)
+        self.assertEqual(SimpleCalculator.add(12558, 95236), 12558 + 95236)
+
     def test_divide(self):
-        self.assertEqual(SimpleCalculator.divide(12,2),6)
-        self.assertEqual(SimpleCalculator.divide(9,0),None)    
+        self.assertEqual(SimpleCalculator.divide(12, 2), 6)
+        # Check if SimpleCalculator handles division by zero correctly
+        with self.assertRaises(ZeroDivisionError):
+            SimpleCalculator.divide(9, 0)
+
+if __name__ == '_main_':
+    unittest.main()
