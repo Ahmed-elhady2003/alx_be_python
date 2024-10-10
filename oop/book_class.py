8ab76d25663fa2b59089cc4c3bbcefbdef484780
ahmed mostafa <ahmedmostafa6464@gmail.com>
2024-10-10 20:39:56 +0300

    bn

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,17 +1,28 @@
 class Book:
-    def __init__(self,title,author,year):
+    def _init_(self, title, author, year):
+        """
+        Initializes a Book instance with the title, author, and publication year.
+        """
         self.title = title
         self.author = author
         self.year = year
-
-
-    def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
-
-    def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
-
-    def __del__(self):
-        self.title.close()
+    
+    def _del_(self):
+        """
+        Prints a message when the object is deleted.
+        """
         print(f"Deleting {self.title}")
-
+    
+    def _str_(self):
+        """
+        Returns a string representation of the Book instance.
+        Example: "1984 by George Orwell, published in 1949"
+        """
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def _repr_(self):
+        """
+        Returns an official string representation that can recreate the Book instance.
+        Example: "Book('1984', 'George Orwell', 1949)"
+        """
+        return f"Book('{self.title}', '{self.author}', {self.year})"
